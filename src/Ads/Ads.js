a5720fa8f59152eae88d4aa3cbeaeb9a2f74e7fa
Daniyal khan <kdaniyal932@gmail.com>
2022-06-29 12:02:35 +0500

    commit

--- src/Ads/Ads.js
+++ src/Ads/Ads.js
@@ -5,35 +5,47 @@ const Ads = () => {
     return (
         <div>
             <div className="form-control upper-ads border-0">
-            <div className="card">
+            <div className="card ads-cards border">
                 <img src={dimg1} className="card-img-top" alt="..."/>
                 <div className="card-body">
                   
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <p >ViVo A10</p>
+                  <p >Memory:256GB</p>
+                  <p >Color : Black</p>
+                  <p >Price : 200$</p>
                  
                 </div>
               </div>
-              <div className="card" >
+              <div className="card ads-cards" >
                 <img src={dimg1} className="card-img-top" alt="..."/>
                 <div className="card-body">
                   
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p >ViVo A10</p>
+                  <p >Memory:256GB</p>
+                  <p >Color : Black</p>
+                  <p >Price : 200$</p>
                  
                 </div>
               </div>
-              <div className="card" >
+              <div className="card ads-cards" >
                 <img src={dimg1} className="card-img-top" alt="..."/>
                 <div className="card-body">
                 
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p >ViVo A10</p>
+                  <p >Memory:256GB</p>
+                  <p >Color : Black</p>
+                  <p >Price : 200$</p>
                  
                 </div>
               </div>
-              <div className="card" >
+              <div className="card ads-cards" >
                 <img src={dimg1} className="card-img-top" alt="..."/>
                 <div className="card-body">
                  
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p >ViVo A10</p>
+                  <p >Memory:256GB</p>
+                  <p >Color : Black</p>
+                  <p >Price : 200$</p>
                  
                 </div>
               </div>
